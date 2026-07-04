cf5cdf2c1ae9616d7d6cb786dca0d36697a2fe84
matinnava <matinkeyt@gmail.com>
2026-07-04 16:11:13 +0330

    Update settings.py

--- backend2/aramti_backend/settings.py
+++ backend2/aramti_backend/settings.py
@@ -16,6 +16,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
     'rest_framework',
